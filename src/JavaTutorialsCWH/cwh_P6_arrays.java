0c3057ca1f7a0bec0a91e942d52bb551e06ebb54
Nijit Gaur <nijitgaur2865@gmail.com>
2025-07-30 23:33:27 +0530

    Commit

--- src/JavaTutorialsCWH/cwh_P6_arrays.java
+++ src/JavaTutorialsCWH/cwh_P6_arrays.java
@@ -86,7 +86,21 @@ public class cwh_P6_arrays {
 
 
         //Q8
-        
+        int[] arr = {1,2,3,5,6,7,8,9};
+        boolean issorted =true;
+        for(int i =0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                issorted =false;
+                break;
+            }
+        }
+        if(issorted){
+            System.out.println("The array is sorted");
+        }
+        else{
+            System.out.println("The array is not sorted");
+        }
+
 
 
 
